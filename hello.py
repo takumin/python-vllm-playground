bc81622b86ab11c72a2fa72c4f703d14cf61b6cf
Takumi Takahashi <takumiiinn@gmail.com>
2025-03-11 02:10:04 +0900

    initial commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+from vllm import LLM, SamplingParams
+
+
+def main():
+    prompts = [
+        "Hello, my name is",
+        "The president of the United States is",
+        "The capital of France is",
+        "The future of AI is",
+    ]
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+    llm = LLM(model="facebook/opt-125m", enforce_eager=True)
+    outputs = llm.generate(prompts, sampling_params)
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+
+if __name__ == "__main__":
+    main()
